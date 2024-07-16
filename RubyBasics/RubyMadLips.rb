4b05732ba007ff3511a687e1dcbe78877c92f394
Saksham (SK) Trivedi) <trivedisaksham@gmail.com>
2024-07-16 15:40:35 +0530

    Mad Lips Game in Ruby

---
+++ RubyBasics/RubyMadLips.rb
@@ -0,0 +1,18 @@
+# Author: Saksham (SK) Trivedi
+# Desciption: Mad lips game in Ruby
+
+puts ("Enter Color1:")
+color1 = gets.chomp.to_s
+
+puts ("Enter Plural Pronoun:")
+PluralPronon = gets.chomp.to_s
+
+puts ("Enter Color2:")
+color2 = gets.chomp.to_s
+
+puts ("Enter Celeb Crush:")
+celeb_crush = gets.chomp.to_s
+
+puts ("Roses are " + color1)
+puts (PluralPronon + " are " + color2)
+puts ("I Love " + celeb_crush)
