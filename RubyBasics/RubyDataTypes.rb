a38ddac6e2883c6d84c9825d6e7bee587ccbd856
Saksham (SK) Trivedi) <trivedisaksham@gmail.com>
2024-07-16 12:09:46 +0530

    Basic Data Types in Ruby

---
+++ RubyBasics/RubyDataTypes.rb
@@ -0,0 +1,26 @@
+# Author: Saksham (SK) Trivedi
+# Description: Below are some Basic Data Types in Ruby
+
+# Integer
+var_int = 75
+
+# String
+var_str = "String"
+
+# Floating Pointer
+var_float = 7.9
+
+# Boolean
+isMale = true
+isFemale = false
+
+# Null
+var_null = nil
+
+puts (var_int)
+puts (var_str)
+puts (var_float)
+puts (isMale)
+puts (isFemale)
+puts (var_null)
+puts "This is the END"
