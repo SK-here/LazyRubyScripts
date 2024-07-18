512f48762720cccb16196a72b1cb138f25d744ed
Saksham (SK) Trivedi) <trivedisaksham@gmail.com>
2024-07-18 17:58:30 +0530

    For Loop

---
+++ RubyBasics/RubyLoops/ForLoop.rb
@@ -0,0 +1,24 @@
+# Author: Saksham (SK) Trivedi
+# Description: For loop in Ruby
+# For loop iterates over the obejct or a sequence such as range or a collection (array)
+
+# Example 0
+for i in 0..4
+  puts i
+end
+
+# Example 1
+SampleSquenece = [1,2,3,4,5,6]
+
+for index in SampleSquenece
+  puts index
+end
+
+# Example 2
+Alphabets = 'a'..'e'
+
+for alphabet in Alphabets
+  puts alphabet
+end
+
+# 
