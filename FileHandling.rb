0a57e79c2f423a6842214c22c0c9487cf7e14cd9
Saksham (SK) Trivedi) <trivedisaksham@gmail.com>
2024-07-24 00:26:34 +0530

    Added few more methods

--- FileHandling.rb
+++ FileHandling.rb
@@ -2,12 +2,12 @@
 # Description: File Handling (Reading from file and Writing to the file)
 
 # Reading from a file
-file_path = 'E:\workbench\LazyRubyScripts\TestFiles\TheDeadStar.txt'
+file_path = 'TestFiles\TheDeadStar.txt'
 
-File.open(file_path, 'r') do |file|
+File.open(file_path, 'r') do |file| # File opens here
   content = file.read
   puts content
-end
+end # file closes here
 
 # 'r'  : Read-only mode (file must exist).
 # 'w'  : Write-only mode (creates a new file or truncates an existing file to zero length).
@@ -16,4 +16,34 @@ end
 # 'w+' : Read-write mode (creates a new file or truncates an existing file to zero length).
 # 'a+' : Read-write mode (appends or creates a new file).
 
+# Another Method to open a file here Line by line
+# This method can be used in case where operations needs to be performed on lines
+File.open(file_path, 'r') do |file|
+  # Read all Lines
+  file.each_line do |line|
+    puts line
+  end
+end
 
+# One more method to open file
+File.open(file_path, 'r') do | file |
+  puts file.read
+end
+
+# Writing to a file
+file_path = 'TestFiles\Writing_mode.txt'
+
+# Opens a new file if file not exists or truncate an existing file to zero
+File.open(file_path, 'w') do |file|
+  file.puts "This is a test"
+end
+
+# Read Write mode (File must exist)
+File.open(file_path, 'r+') do |file|
+  puts file.read
+  counter = 1
+  file.puts "This is #{counter += 1}  line"
+  puts file.read
+end
+
+# 
